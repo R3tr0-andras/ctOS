57d11d2c032b8d0a78f1679d95c1f5bbc3786890
Andras-corda <122601492+Andras-corda@users.noreply.github.com>
2024-01-31 22:44:55 +0100

    .

--- Views/Components/footer.php
+++ Views/Components/footer.php
@@ -1,29 +1,3 @@
-<div class="flex center align-center">
-    <div class="">
-        <h3>Services</h3>
-        <ul>
-            <li><a href="#">Web design</a></li>
-            <li><a href="#">Development</a></li>
-            <li><a href="#">Hosting</a></li>
-        </ul>
-    </div>
-    <div class="">
-        <h3>About</h3>
-        <ul>
-            <li><a href="#">Company</a></li>
-            <li><a href="#">Team</a></li>
-            <li><a href="#">Legacy</a></li>
-        </ul>
-    </div>
-    <div class="">
-        <h3>Careers</h3>
-        <ul>
-            <li><a href="#">Job openings</a></li>
-            <li><a href="#">Employee success</a></li>
-            <li><a href="#">Benefits</a></li>
-        </ul>
-    </div>
-</div>
 <div>
     <p class="copyright">Blume Company © 2012</p>
 </div>
--- Views/home.php
+++ Views/home.php
@@ -1,11 +1,10 @@
-<section>
-    <p>
-        Le Central Operating System, ou ctOS est un système informatisé hautes performances créé par la société Blume qui permet de relier les infrastructures et les installations de sécurité publique de la ville à un pôle d’échange centralisé. Ce système gère principalement la circulation routière, le développement économique, les télécommunications et le contrôle des activités criminelles à l’échelle de la ville de Chicago. Il a permis d’apporter d’importants bénéfices aux habitants, notamment une réduction des temps de transport et du taux de criminalité, des services de voirie améliorés et bien d’autres choses encore.
-    </p>
-</section>
-<section>
-    <iframe width="1280" height="720" 
-    src="https://www.youtube.com/embed/iHF0239a8jo" title="ctOS 2.0 Presentation - FR" 
-    frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-    allowfullscreen></iframe>
-</section>
+<div class="main-home-diplay">
+    <section class="CtosExplain">
+        <p>
+            Le Central Operating System, ou ctOS est un système informatisé hautes performances créé par la société Blume qui permet de relier les infrastructures et les installations de sécurité publique de la ville à un pôle d’échange centralisé. Ce système gère principalement la circulation routière, le développement économique, les télécommunications et le contrôle des activités criminelles à l’échelle de la ville de Chicago. Il a permis d’apporter d’importants bénéfices aux habitants, notamment une réduction des temps de transport et du taux de criminalité, des services de voirie améliorés et bien d’autres choses encore.
+        </p>
+    </section>
+    <section class="class=" CtosExplain"">
+        <iframe width="1280" height="720" src="https://www.youtube.com/embed/iHF0239a8jo" title="ctOS 2.0 Presentation - FR" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </section>
+</div>

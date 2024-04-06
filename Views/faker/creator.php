6ac3921c1f2dd466b4c0e948087272d7227766a8
Andras-corda <122601492+Andras-corda@users.noreply.github.com>
2024-04-06 12:41:08 +0200

    push

--- Views/faker/creator.php
+++ Views/faker/creator.php
@@ -8,26 +8,26 @@
     <!-- Résultats de la recherche -->
     <h2 class="title">Résultats de la recherche :</h2>
     <div>
-            <div>
-                <div class="user-card">
-                    <?php
-                    if ($newfakerResult->userIsFaker == 1) {
-                        $profileImage = "../Assets/Pictures/fakerProfile/" . $newfakerResult->userProfileImage;
-                    } else {
-                        $profileImage = "../Assets/pictures/userProfile/" . $newfakerResult->userProfileImage;
-                    }
-                    ?>
-                    <a href="trackingUser?userId=<?= $result->userId ?>">
-                        <img src="<?= $profileImage; ?>" alt="Image de l'utilisateur" class="user-image">
-                    </a>
-                    <div class="user-info">
-                        <h5>
-                            <a href="trackingUser?userId=<?= $newfakerResult->userId ?>">ID Utilisateur: <?= $newfakerResult->userId; ?></a>
-                        </h5>
-                        <p><span>Nom:</span> <?= $newfakerResult->userName; ?></p>
-                        <p><span>Prénom:</span> <?= $newfakerResult->userFirstName; ?></p>
-                    </div>
+        <div>
+            <div class="user-card">
+                <?php
+                if ($newfakerResult->userIsFaker == 1) {
+                    $profileImage = "../Assets/Pictures/fakerProfile/" . $newfakerResult->userProfileImage;
+                } else {
+                    $profileImage = "../Assets/pictures/userProfile/" . $newfakerResult->userProfileImage;
+                }
+                ?>
+                <a href="trackingUser?userId=<?= $result->userId ?>">
+                    <img src="<?= $profileImage; ?>" alt="Image de l'utilisateur" class="user-image">
+                </a>
+                <div class="user-info">
+                    <h5>
+                        <a href="trackingUser?userId=<?= $newfakerResult->userId ?>">ID Utilisateur: <?= $newfakerResult->userId; ?></a>
+                    </h5>
+                    <p><span>Nom:</span> <?= $newfakerResult->userName; ?></p>
+                    <p><span>Prénom:</span> <?= $newfakerResult->userFirstName; ?></p>
                 </div>
             </div>
+        </div>
     </div>
 <?php endif; ?>
